9286bd980e3a113c95fe6ae5df2c520b9754fc34
MUmerYasin <mumeryasin123456789@gmail.com>
2021-08-02 17:44:29 +0500

    vertical carousel slider, Rotate Images

--- lib/main.dart
+++ lib/main.dart
@@ -100,6 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     autoPlayCurve: Curves.fastOutSlowIn,
                     // onPageChanged: callbackFunction,
                     scrollDirection: Axis.horizontal,
+                    // aspectRatio: 2.0,
                     // reverse = true,
                       ),
                   items: [1, 2, 3, 4, 5, 6].map((i) {
@@ -132,6 +133,72 @@ class _MyHomePageState extends State<MyHomePage> {
                   }).toList(),
 
                 ),
+
+                ),
+              ],
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Expanded(child: Text("vertical Carousel Slider ", style: TextStyle(fontSize: 20.0),)),
+                ),
+                SizedBox(height: 40.0,),
+              ],
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: CarouselSlider(
+                    options: CarouselOptions(
+                      height: 200.0,
+                      enableInfiniteScroll: true,
+                      // enableInfiniteScroll = true,
+                      autoPlay: true,
+                      enlargeCenterPage: true,
+                      autoPlayInterval: const Duration(seconds: 2),
+                      autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                      autoPlayCurve: Curves.fastOutSlowIn,
+                      // onPageChanged: callbackFunction,
+                      scrollDirection: Axis.vertical,
+                      // aspectRatio: 2.0,
+                      // reverse = true,
+                    ),
+                    items: [1, 2, 3, 4, 5, 6].map((i) {
+                      return Builder(
+                        builder: (BuildContext context) {
+                          return Container(
+                              width: MediaQuery.of(context).size.width,
+                              margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                              decoration: const BoxDecoration(
+                                color: Colors.transparent,
+                              ),
+
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Expanded(
+                                    child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(20.0),
+                                        child: Image.asset(
+                                            'assets/imagesall/$i.jpg')),
+                                  ),
+                                  Text(
+                                    'Image Name: $i',
+                                    style: const TextStyle(fontSize: 20.0),
+                                  ),
+                                ],
+                              ));
+                        },
+                      );
+                    }).toList(),
+
+                  ),
 
                 ),
               ],
