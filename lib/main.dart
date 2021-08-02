ca4b5a354c849eeb84a80b64754a5d4d3924e152
MUmerYasin <mumeryasin123456789@gmail.com>
2021-08-02 17:31:02 +0500

    carousel slider, Rotate Images

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -24,8 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-
-
   final String title;
 
   @override
@@ -33,25 +31,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   double _currentSliderValue = 20;
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         title: Text(widget.title),
       ),
       body: SingleChildScrollView(
-
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-                    Row(
-                      children: [
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text("Slider ", style: TextStyle(fontSize: 20.0),),
+                ),
+              ],
+            ),
+            Row(
+              children: [
                 Expanded(
                   child: Slider(
                     value: _currentSliderValue,
@@ -69,10 +72,73 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
 
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text("Carousel Slider ", style: TextStyle(fontSize: 20.0),),
+                ),
+              ],
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                    child: CarouselSlider(
+                  options: CarouselOptions(
+                    height: 200.0,
+                      enableInfiniteScroll: true,
+                    // enableInfiniteScroll = true,
+                    autoPlay: true,
+                    enlargeCenterPage: true,
+                      autoPlayInterval: const Duration(seconds: 2),
+                      autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    // onPageChanged: callbackFunction,
+                    scrollDirection: Axis.horizontal,
+                    // reverse = true,
+                      ),
+                  items: [1, 2, 3, 4, 5, 6].map((i) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                            decoration: const BoxDecoration(
+                              color: Colors.transparent,
+                            ),
+
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Expanded(
+                                  child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                      child: Image.asset(
+                                          'assets/imagesall/$i.jpg')),
+                                ),
+                                Text(
+                                  'Image Name: $i',
+                                  style: const TextStyle(fontSize: 20.0),
+                                ),
+                              ],
+                            ));
+                      },
+                    );
+                  }).toList(),
+
+                ),
+
+                ),
+              ],
+            ),
           ],
         ),
       ),
-
     );
   }
 }
