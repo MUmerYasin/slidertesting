d2947738f7ce5b0d482836faa1958e8245d1d712
MUmerYasin <mumeryasin123456789@gmail.com>
2021-08-03 18:03:23 +0500

    App bar Editing,add Drop down menu

--- lib/appbartesting.dart
+++ lib/appbartesting.dart
@@ -9,13 +9,16 @@ class AppBarTestingC extends StatefulWidget {
 
 class _AppBarTestingCState extends State<AppBarTestingC> {
   final _formKey = GlobalKey<FormState>();
+  String dropdownValue = 'One';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
         backgroundColor: Colors.deepPurple,
         elevation: 20.0,
-          leadingWidth: 400.0,
+        leadingWidth: 700.0,
         leading: Form(
           key: _formKey,
           child: Column(
@@ -23,27 +26,36 @@ class _AppBarTestingCState extends State<AppBarTestingC> {
             children: [
               Row(
                 children: [
-                  Expanded(
+                  Flexible(
+                    fit: FlexFit.loose,
                     child: Container(
-                      margin: const EdgeInsets.only(left: 8.0,top: 4.0, bottom: 4.0,),
-
+                      margin: const EdgeInsets.only(
+                        left: 8.0,
+                        top: 4.0,
+                        bottom: 4.0,
+                      ),
                       child: TextFormField(
-
                         // textAlign: TextAlign.left,
                         // expands: true,
-                        style: const TextStyle(fontSize: 20.0,  color: Colors.white,),
+                        style: const TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
                         //letterSpacing: 2,
 
-
                         decoration: const InputDecoration(
-                            isDense: true, // important line
-                            contentPadding: EdgeInsets.all(8.0),
+                          isDense: true,
+                          // important line
+                          contentPadding: EdgeInsets.all(8.0),
 
-                          hintStyle: TextStyle( fontSize: 20.0,color: Colors.white,),
-                          fillColor:  Colors.white38 ,
+                          hintStyle: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white,
+                          ),
+                          fillColor: Colors.white38,
                           filled: true,
 
-                            hintText: 'Search',
+                          hintText: 'Search',
                           hoverColor: Colors.white,
 
                           suffixIcon: Padding(
@@ -55,17 +67,14 @@ class _AppBarTestingCState extends State<AppBarTestingC> {
                             ), // icon is 48px widget.
                           ),
 
-
-                          enabledBorder:  OutlineInputBorder(
+                          enabledBorder: OutlineInputBorder(
                             // width: 0.0 produces a thin "hairline" border
-                            borderSide:  BorderSide(color: Colors.white, width: 2.0),
-
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 2.0),
                           ),
-                          border:  OutlineInputBorder(),
+                          border: OutlineInputBorder(),
                           // labelStyle: new TextStyle(color: Colors.green),
-
-                            ),
-
+                        ),
 
                         // The validator receives the text that the user has entered.
                         validator: (value) {
@@ -82,109 +91,94 @@ class _AppBarTestingCState extends State<AppBarTestingC> {
             ],
           ),
         ),
+        automaticallyImplyLeading: true,
 
         // title: const Text('app bar testing'),
 
-          // title: Container(
-        //   child: Form(
-        //     key: _formKey,
-        //     child: Column(
-        //       crossAxisAlignment: CrossAxisAlignment.start,
-        //       children: [
-        //         Expanded(
-        //           child: TextFormField(
-        //             // decoration: InputDecoration(
-        //             //     border: UnderlineInputBorder(
-        //             //         borderRadius:BorderRadius.circular(5.0)),
-        //             //     hintText: 'Search'
-        //             // labelText: 'Enter something',
-        //             // ),
-        //
-        //             decoration: const InputDecoration(
-        //               hintText: 'Search',
-        //               border: OutlineInputBorder(),
-        //               //   border: OutlineInputBorder(
-        //               //   borderSide: BorderSide(
-        //               //       color: Colors.red,
-        //               //       width: 5.0),
-        //               // ),
-        //               // enabledBorder: OutlineInputBorder(
-        //               //   borderSide: const BorderSide(width: 3, color: Colors.blue),
-        //               //   borderRadius: BorderRadius.circular(15),
-        //               // ),
-        //               // focusedBorder: OutlineInputBorder(
-        //               //   borderSide: const BorderSide(width: 3, color: Colors.red),
-        //               //   borderRadius: BorderRadius.circular(15),
-        //               // ),
-        //             ),
-        //
-        //
-        //             // The validator receives the text that the user has entered.
-        //             validator: (value) {
-        //               if (value == null || value.isEmpty) {
-        //                 return 'Please enter some text';
-        //               }
-        //               return null;
-        //             },
-        //           ),
-        //         ),
-        //         // Padding(
-        //         //   padding: const EdgeInsets.symmetric(vertical: 16.0),
-        //         //   child: ElevatedButton(
-        //         //     onPressed: () {
-        //         //       // Validate returns true if the form is valid, or false otherwise.
-        //         //       if (_formKey.currentState!.validate()) {
-        //         //         // If the form is valid, display a snackbar. In the real world,
-        //         //         // you'd often call a server or save the information in a database.
-        //         //         ScaffoldMessenger.of(context).showSnackBar(
-        //         //           const SnackBar(content: Text('Processing Data')),
-        //         //         );
-        //         //       }
-        //         //     },
-        //         //     child: const Text('Submit'),
-        //         //   ),
-        //         // ),
-        //       ],
-        //     ),
-        //   ),
-        // ) ,
 
         actions: <Widget>[
 
+          Flexible (
+      fit :FlexFit.loose,
+            child: IconButton(
+              icon: const Icon(Icons.add_alert),
 
-
-          IconButton(
-            icon: const Icon(Icons.add_alert),
-            tooltip: 'Show Snackbar',
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('This is a snackbar')));
-            },
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Bell is Ringing')));
+              },
+            ),
+          ),
+          Flexible (
+            fit :FlexFit.loose,
+            child: DropdownButton<String>(
+              value: dropdownValue,
+              icon: const Icon(Icons.arrow_downward,color: Colors.white,),
+              iconSize: 24,
+              // elevation: 24,
+              style: const TextStyle(color: Colors.white, fontSize: 20.0),
+              underline: Container(
+                height: 3,
+                color: Colors.deepPurple,
+              ),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownValue = newValue!;
+                });
+              },
+              items: <String>['One', 'ENG', 'PAK', 'TUK']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
           ),
           // IconButton(
-          //   icon: const Icon(Icons.navigate_next),
-          //   tooltip: 'Go to the next page',
+          //   icon: const Icon(Icons.view_headline_sharp),
+          //
           //   onPressed: () {
-          //     Navigator.push(context, MaterialPageRoute<void>(
-          //       builder: (BuildContext context) {
-          //         return Scaffold(
-          //           appBar: AppBar(
-          //             title: const Text('Next page'),
-          //           ),
-          //           body: const Center(
-          //             child: Text(
-          //               'This is the next page',
-          //               style: TextStyle(fontSize: 24),
-          //             ),
-          //           ),
-          //         );
-          //       },
-          //     ));
+          //     Scaffold.of(context).openEndDrawer();
           //   },
           // ),
         ],
-
       ),
+      endDrawer: Drawer(
+        elevation: 10.0,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+
+          ],
+        ),
+      ),
+
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
